03ed0095065533d3ec3e83bb9c505dc243c54fd8
Nick Ebbitt <nick.ebbitt@tracsis.com>
2016-09-14 22:12:32 +0100

    Add Maven failsafe plugin and example integration-test

---
+++ language-processor/src/test/java/it/LanguageProcessorIT.java
@@ -0,0 +1,14 @@
+package it;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertTrue;
+
+public class LanguageProcessorIT {
+
+    @Test
+    public void thisIsATestIT() {
+        assertTrue(false);
+    }
+
+}
